e05d8a9d48f6f4b2417e4f9bdb7d2781d81eb9ec
Ariel Don <ariel@arieldon.com>
2022-10-17 08:30:45 -0500

    Update compilation script

--- compile.sh
+++ compile.sh
@@ -5,7 +5,4 @@ set -eux
 DEBUG="-DDEBUG -g -O0"
 WARNINGS="-Wall -Wextra -Wpedantic"
 
-MEMORY="-fsanitize=alignment,bounds,array-bounds,local-bounds,memory,pointer-overflow,undefined -fsanitize-memory-track-origins -fno-omit-frame-pointer"
-ADDRESS="-fsanitize=address,alignment,bounds,array-bounds,local-bounds,pointer-overflow,undefined -fno-omit-frame-pointer"
-
-gcc -D_FORTIFY_SOURCE=2 $WARNINGS -Iinclude/ src/* -o dnsresolver
+gcc -D_FORTIFY_SOURCE=2 $DEBUG $WARNINGS -Iinclude/ src/* -o dnsresolver
